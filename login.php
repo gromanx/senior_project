7ed96d6c7c29a8711e666360e595ac79ed8ccf18
Lavinew <55567171+Lavinew@users.noreply.github.com>
2020-02-24 19:12:45 -0800

    login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php 
+$username=$_POST['username'];
+$password=$_POST['pass'];
+$link=mysqli_connect("96.44.135.40","valkyrie_005781551","GiKC4-2OTwok","valkyrie_scrambeledlegs");
+$query=mysqli_query($link,"SELECT Email,Password FROM Users WHERE Email = '$username'");
+$result=mysqli_query($con,$sql);
+$row = mysqli_fetch_array($query);
+if($_POST['submit']){    
+    if($row['Email']==$username && $row['Password']==$password){
+        setcookie('uname',$username,time()+7200);
+        echo "<script>alert('successfully');window.location= 'map.html';</script>";
+    }
+    else 
+       {
+         
+         echo "<script>alert('failed');history.go(-1)</script>";
+         
+       }
+         
+}
+include('login.html');
+?>
